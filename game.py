3b39918eb5c8fd4a19f4f0c8e63dea4c1ca2b96a
scottsc0de <scott5902@gmail.com>
2023-10-25 11:39:15 -0400

    main game if statements

--- game.py
+++ game.py
@@ -13,5 +13,42 @@ def startGame():
     userMove = input("\nPlease type your move: Rock, Paper, or Scissors: \n")
     computerMove = random.choice(["Rock", "Paper", "Scissors"])
 
+    print(f"\nUser: {userMove}")
+    print(f"Computer: {computerMove}\n")
+
+    if (
+        userMove == "Rock"
+        and computerMove == "Paper"
+        or userMove == "Scissors"
+        and computerMove == "Rock"
+        or userMove == "Paper"
+        and computerMove == "Scissors"
+    ):
+        print("Computer wins")
+        computerScore += 1
+
+        print(userScore)
+        print(computerScore)
+
+    elif (
+        userMove == "Paper"
+        and computerMove == "Rock"
+        or userMove == "Rock"
+        and computerMove == "Scissors"
+        or userMove == "Scissors"
+        and computerMove == "Paper"
+    ):
+        print("User wins")
+        userScore += 1
+
+        print(userScore)
+        print(computerScore)
+
+    else:
+        print("Its a draw")
+
+        print(userScore)
+        print(computerScore)
+
 
 startGame()
